35beea78ee5902a97276496c35e39821caf25d90
hpal <mehimu@gmail.com>
2017-07-14 00:40:29 -0500

    some optimization and pushing - using only plain sql inside spark.sql("") - another way

--- src/main/scala/com/coding/de/AppleMainSql.scala
+++ src/main/scala/com/coding/de/AppleMainSql.scala
@@ -5,7 +5,6 @@ import java.util.regex.Pattern
 import com.coding.de.model.ModelFactory._
 import com.coding.de.util.Util
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.storage.StorageLevel
 
@@ -13,7 +12,7 @@ import org.apache.spark.storage.StorageLevel
   * Created by Himadri Pal (mehimu@gmail.com)
   * Extend Util trait to have all the utility methods and constants and enums available to this object
   */
-object AppleMain extends  Util{
+object AppleMainSql extends  Util{
 
   def main(args : Array[String]) = {
 
@@ -35,7 +34,7 @@ object AppleMain extends  Util{
                     /* convert all locale formatted string to double in df */   convertToDouble(rowArr(PRODUCT.PRODUCT_PRICE.id))
                                                                 )
                                                       ).toDF().cache();
-
+      prod_df.createOrReplaceTempView("product")
     //load Sales.txt file in salesRaw rdd
     val sales_raw = spark.sparkContext.textFile(dataDir+"/Sales.txt")
     //convert salesRaw to a data frame using Sales case class attirbute names as column names.
@@ -47,7 +46,9 @@ object AppleMain extends  Util{
                                /*convert each locale formated string to double*/        convertToDouble(rowArr(SALES.TOTAL_AMOUNT.id)),
                                                                                         rowArr(SALES.TOTAL_QUANTITY.id).toInt
                                                                                         )
-      ).toDF().persist(StorageLevel.MEMORY_AND_DISK);
+      ).toDF() //.persist(StorageLevel.MEMORY_AND_DISK);
+
+    sales_df.createOrReplaceTempView("sales")
     /*Persisting as it will be used later, intentional persist instead of cache to specify spark to use both memory and disk */
 
     //join product and sales data and find out sales distribution by product name and product type
@@ -55,10 +56,12 @@ object AppleMain extends  Util{
     // output can be saved if the requirement be.
     println("==============================================================================================================================")
     println("2. distribution of sales by product name and product type.")
-    val sales_distribution = sales_df.join(prod_df, SALES.PRODUCT_ID.toString)
+    /*val sales_distribution = sales_df.join(prod_df, SALES.PRODUCT_ID.toString)
                                         .groupBy(PRODUCT.PRODUCT_NAME.toString,PRODUCT.PRODUCT_TYPE.toString)
-                                        .agg(sum(SALES.TOTAL_AMOUNT.toString).alias(SALES.TOTAL_AMOUNT.toString)).orderBy(desc(SALES.TOTAL_AMOUNT.toString)).show(25)
+                                        .agg(sum(SALES.TOTAL_AMOUNT.toString).alias(SALES.TOTAL_AMOUNT.toString)).orderBy(desc(SALES.TOTAL_AMOUNT.toString)).show(25)*/
 
+    val sales_distribution = spark.sql("select p.product_name,p.product_type,sum(s.total_amount) as total_sales from product p left outer join " +
+      "sales s on s.product_id=p.product_id group by p.product_name,p.product_type order by total_sales desc").show(30)
     //orderby is not in the question but added it to give more meaning the dat
     // 25 -> as there are only those many product name and type combinations.
     println("==============================================================================================================================")
@@ -75,7 +78,7 @@ object AppleMain extends  Util{
                                                                       rowArr(REFUND.REFUND_QUANTITY.id).toInt
                                                                       )
     ).toDF()
-
+    refund_df.createOrReplaceTempView("refund")
 
     val customer_raw = spark.sparkContext.textFile(dataDir+"/Customer.txt")
     val customer_df = customer_raw.map(row => row.split(Pattern.quote(Constant.PIPE))).map(rowArr => Customer(
@@ -85,78 +88,31 @@ object AppleMain extends  Util{
                                                                               rowArr(CUSTOMER.PHONE_NUMBER.id)
                                                                                 )
                                                               ).toDF();
-
+    customer_df.createOrReplaceTempView("customer")
     println("===============================================================================================================================")
     println("3. Total amount of all transactions that happened in year 2013 and have not been refunded as of today.")
 
-    //to work with only 2013 the following two queries
-    //persisting/caching both sales and refund as these two will be used for the next query
-    val sales_2013 = sales_df.filter(year(sales_df(SALES.TIMESTAMP.toString))===lit("2013")).persist(StorageLevel.MEMORY_AND_DISK)
-    //refund should be in 2013 or beyond, caching for next use
-    val refund_2013_and_beyond = refund_df.filter(year(refund_df(SALES.TIMESTAMP.toString))>=lit("2013")).
-                                          select(REFUND.ORIGINAL_TRANSACTION_ID.toString,
-                                                  REFUND.REFUND_AMOUNT.toString)
-                                          .persist(StorageLevel.MEMORY_AND_DISK);
+    val sales_wt_no_rfnd = spark.sql("select sum(a.total) as total_sales_with_no_refund from (select s.total_amount as total from sales s left outer join refund r on " +
+      "s.transaction_id=r.original_transaction_id where year(s.timestamp)==2013 and r.original_transaction_id is null) a").show()
 
-    //left outer join with refund and cache it
-    val sales_and_refund_join = sales_2013.join(refund_2013_and_beyond,
-                                                sales_2013(SALES.TRANSACTION_ID.toString)===refund_2013_and_beyond(REFUND.ORIGINAL_TRANSACTION_ID.toString),
-                                                Constant.LEFT_OUTER).persist(StorageLevel.MEMORY_AND_DISK);
 
-    //as left outer join was used, all matching rows from sales_df will come with all columns for refund_df as null
-    // hence all transactions ids which does not have a refund will have original_transaction_id as null,
-    // hence filtering all rows where ORIGINAL_TRANSACTION_ID is null
-    val sales_no_refund_2013= sales_and_refund_join.where(refund_2013_and_beyond(REFUND.ORIGINAL_TRANSACTION_ID.toString).isNull).select(SALES.TOTAL_AMOUNT.toString)
-    //its efficient to use internal rdd of the data frame for map and reduce
-    //need total of all rows from previous dataframe
-    val total_sales_no_refund = sales_no_refund_2013.rdd.map(_(0).asInstanceOf[Double]).reduce(_+_)
-
-    println("total = "+total_sales_no_refund)
     println("===============================================================================================================================")
 
 
     println("===============================================================================================================================")
     println("4. customer name who made the second most purchases in the month of May 2013. Refunds be excluded")
-    //from sales_and_refund cached dataframe filter the records for the month of may
-    //fill all null refund_amount column with 0.0
-    //derive a column net_sales = sales_df(total_amount) - refund_df(refund_amount)
-    val sales_and_refund_2013_may = sales_and_refund_join.filter(month(sales_and_refund_join(SALES.TIMESTAMP.toString))===lit("5"))
-                                  .na.fill(0.0,Seq(REFUND.REFUND_AMOUNT.toString))
-                                  .withColumn(Constant.NET_SALES, $"total_amount" - $"refund_amount") //could have used names from ENUM
+    val cust_with_2nd_most_purchase = spark.sql("select concat_ws(' ',customer_first_name,customer_last_name) as name,net_sales,dense_rank from " +
+      "(select sum(s.total_amount - coalesce(r.refund_amount,0L)) as net_sales,s.customer_id as customer_id," +
+      "dense_rank() over(order by sum(s.total_amount - coalesce(r.refund_amount,0L)) desc) as dense_rank " +
+      "from sales s left outer join refund r on s.transaction_id=r.original_transaction_id where year(s.timestamp)=2013 and month(s.timestamp)=5 group by s.customer_id) a " +
+      "inner join customer c on a.customer_id=c.customer_id where dense_rank=2").show()
 
-    //group by customer_id and aggregate  using sum
-    // total_sales sorted on desc order, most purchase at the top
-    val w = Window.orderBy(desc("net_total"))
-    val per_customer_sale = sales_and_refund_2013_may.groupBy(SALES.CUSTOMER_ID.toString)
-                                                    .agg(sum(Constant.NET_SALES).alias(Constant.NET_SALES))
-                                                    .withColumn("dense_rank", dense_rank().over(w)).where("dense_rank==2").join(customer_df,"customer_id")
-                                                    .select(concat_ws(" ",$"customer_first_name",$"customer_last_name"),$"net_total")show()
-
-
-   /* //head(2) will take first two and last (2nd row) will be the second most
-    // extract customer_id from the last (2nd row)
-    val customer_id_with_2nd_most_purchase :String  = per_customer_sale.head(2).last.getAs[String](SALES.CUSTOMER_ID.toString)
-
-    //retrive first_name ,last_name concat with space for the customer_id calculated above
-    //using show for nice tabular format and its only 1 row
-    val cust_details_with_2nd_most_purchase = customer_df.select(concat_ws(Constant.SPACE,
-                                                                          customer_df(CUSTOMER.CUSTOMER_FIRST_NAME.toString),
-                                                                          customer_df(CUSTOMER.CUSTOMER_LAST_NAME.toString)
-                                                                          ).alias(Constant.CUSTOMER_NAME)
-                                                                ).where(CUSTOMER.CUSTOMER_ID.toString+"="+ customer_id_with_2nd_most_purchase).show()*/
-
-    println("===============================================================================================================================")
 
 
     println("================================================================================================================================")
     println("5. product that has not been sold at least once (if any).")
-    //join product with sales df with inner join
-    //left outer join prod_df with sales_df will associate all transactions for each product id, if there is a product which does not have record in sales
-    // then transaction id will be null, those are the products not sold at least once.
-    /*val prod_with_no_sale = prod_df.join(sales_df,
-                                        prod_df(PRODUCT.PRODUCT_ID.toString)===sales_df(SALES.PRODUCT_ID.toString),Constant.LEFT_OUTER).
-                                        where(sales_df(SALES.TRANSACTION_ID.toString).isNull).show()*/
-    val prod_with_no_sale= sales_df.join(broadcast(prod_df),$"product_id"===$"product_id",Constant.LEFT_OUTER).where($"transaction_id".isNull).show()
+
+    val prod_with_no_sale = spark.sql("select p.* from product p left outer join sales s on p.product_id=s.product_id where s.transaction_id is null")
 
     println("===============================================================================================================================")
 
@@ -164,20 +120,12 @@ object AppleMain extends  Util{
     println("===============================================================================================================================")
     println(" 6 . total number of users who purchased the same product consecutively at least 2 times on a given day.")
     // format the date without time, seect customer id and product id as well
-    val cust_prod_sale_day = sales_df.select(sales_df(SALES.CUSTOMER_ID.toString),
-                                            sales_df(SALES.PRODUCT_ID.toString),
-                                            date_format(sales_df(SALES.TIMESTAMP.toString),"MM/dd/yyyy").alias(Constant.SALE_DAY))
-
     //group by sale_day,product_id and customer_id having count of those records >=2  meaning at least twice
     // Assumption : refind has not been taken into account , meaning if a person bought and then refunded on the same day, it will still count as one purchase
     // on that day
-     val total_cust_with_2_sale_a_day = cust_prod_sale_day.groupBy(Constant.SALE_DAY,
-                                                                  SALES.PRODUCT_ID.toString,
-                                                                  SALES.CUSTOMER_ID.toString)
-                                                                  .agg(count("*").alias("count"))
-                                                                  .where($"count" >=2).count()
+    val total_cust_with_2_sale_a_day = spark.sql("select count(*) as total_cust_with_sales_twice_a_day from (select count(*) as count,date_format(timestamp, 'MM/dd/yyyy') as day,product_id,customer_id from sales " +
+    "group by date_format(timestamp, 'MM/dd/yyyy'),product_id,customer_id having count >=2) a").show()
 
-    println("   total customers - "+total_cust_with_2_sale_a_day)
     println("==============================================================================================================================")
 
 
@@ -216,7 +164,8 @@ object AppleMain extends  Util{
                                                                 )
                                                        ).toDF()
     //we need to search only on current_address also both column and value is switch cased to UPPER. used like but = would also work for this case
-val customer_at_addr = customer_extended_df.filter(upper($"current_address.street_address").like("1154 Winters Blvd".toUpperCase)).show();
+ customer_extended_df.createOrReplaceTempView("customer_extnd")
+ val cust_at_addr=spark.sql("select * from customer_extnd where upper(current_address.street_address)=upper('1154 Winters Blvd')").show()
 println("===============================================================================================================================")
 }
 
